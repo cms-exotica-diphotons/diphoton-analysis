81f112e6b585abc68bd90797bfb32df82af1e91d
Chris West <christopher.alan.west@gmail.com>
2022-05-29 13:23:12 -0500

    Include commands for real photon contamination calculation

--- Tools/commands.py
+++ Tools/commands.py
@@ -3,12 +3,15 @@
 Fit histogram templates for diphoton analysis
 '''
 years = [2016, 2017, 2018]
-datasets = ["average", "jetht", "doublemuon"]
+# "gg" fakes are used for real photon contamination estimates
+datasets = ["gg", "data"]
+fake_types = ["average", "jetht", "doublemuon"]
 regions = ["BB", "BE"]
 regions_alternate = ["barrel", "endcap"]
 for year in years:
     for dataset in datasets:
-            arguments = "data " + str(year) +  " " + dataset
+        for fake_type in fake_types:
+            arguments = dataset + " " + str(year) +  " " + fake_type
             print("getFakePrediction.exe " + arguments)
 
 for year in years:
@@ -24,10 +27,10 @@ for year in years:
 
 for year in years:
     for region in regions_alternate:
-            arguments = region + " " + str(year)
-            print("datadriven.exe " + arguments)
+        arguments = region + " " + str(year)
+        print("datadriven.exe " + arguments)
 
 for year in years:
     for region in regions_alternate:
-            arguments = region + " " + str(year)
-            print("basicplots.exe " + arguments)
+        arguments = region + " " + str(year)
+        print("basicplots.exe " + arguments)
